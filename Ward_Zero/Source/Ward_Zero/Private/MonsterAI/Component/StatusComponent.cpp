d4aacf3b6d94071ed82a9dfc4bfbb8c615f64467
Hoseok-hth <heart12500@gmail.com>
2026-02-04 15:51:17 +0900

    아무것도 된게 없지만 일단 올려버림

---
+++ Ward_Zero/Source/Ward_Zero/Private/MonsterAI/Component/StatusComponent.cpp
@@ -0,0 +1,104 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MonsterAI/Component/StatusComponent.h"
+#include "Public/MonsterAI/Data/MonsterDataAsset.h"
+
+UStatusComponent::UStatusComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UStatusComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void UStatusComponent::InitData(const UMonsterDataAsset* BaseData)
+{
+	if (BaseData)
+	{
+		MonsterData = BaseData;
+		BaseSpeed = BaseData->BaseSpeed;
+		ChaseSpeed = BaseData->ChaseSpeed;
+		CurrentHP = BaseData->MaxHP;
+	}
+}
+
+
+float UStatusComponent::GetBaseSpeed() const
+{
+	return BaseSpeed;
+}
+
+void UStatusComponent::SetBaseSpeed(float speed)
+{
+	BaseSpeed = speed;
+}
+
+float UStatusComponent::GetChaseSpeed() const
+{
+	return ChaseSpeed;
+}
+
+void UStatusComponent::SetChaseSpeed(float speed)
+{
+	ChaseSpeed = speed;
+}
+
+float UStatusComponent::GetArrivalRadius() const
+{
+	return MonsterData->ArrivalRadius;
+}
+
+float UStatusComponent::GetBaseDetectionRange() const
+{
+	return MonsterData->BaseDetectionRange;
+}
+
+float UStatusComponent::GetViewAngle() const
+{
+	return MonsterData->ViewAngle;
+}
+
+float UStatusComponent::GetAttackRange() const
+{
+	return MonsterData->AttackRange;
+}
+
+float UStatusComponent::GetChaseRange() const
+{
+	return MonsterData->ChaseRange;
+}
+
+float UStatusComponent::GetEyeHeight() const
+{
+	return MonsterData->EyeHeight;
+}
+
+float UStatusComponent::GetMaxLostTargetTime() const
+{
+	return MonsterData->MaxLostTargetTime;
+}
+
+float UStatusComponent::GetIdleSoundVolume() const
+{
+	return MonsterData->IdleSoundVolume;
+}
+
+float UStatusComponent::GetChaseSoundVolume() const
+{
+	return MonsterData->ChaseSoundVolume;
+}
+
+
+float UStatusComponent::GetStunTime() const
+{
+	return MonsterData->StunnedTime;
+}
+
+float UStatusComponent::GetHearingThreshold() const
+{
+	return MonsterData->HearingThreshold;
+}
---
+++ Ward_Zero/Source/Ward_Zero/Public/MonsterAI/Entity/BaseZombie.h
@@ -0,0 +1,64 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseZombie.generated.h"
+
+class UStatusComponent;
+
+UCLASS()
+class WARD_ZERO_API ABaseZombie : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseZombie();
+
+virtual void Tick(float DeltaSeconds) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
+	const UMonsterDataAsset* MonsterData;
+	
+	UAudioComponent* GetAudioLoopComponent() const { return AudioLoopComponent; }
+	
+	UFUNCTION(CallInEditor, Category = "Data")
+	void RefreshMonster();
+	
+	UFUNCTION(BlueprintCallable, Category = "Status", meta=(DisplayName="Get Monster Base Speed"))
+	float GetBaseSpeed();
+	
+	UFUNCTION(BlueprintCallable, Category = "Status", meta=(DisplayName="Set Monster Base Speed"))
+	void SetBaseSpeed(float NewSpeed);
+	
+	UFUNCTION(BlueprintCallable, Category = "Status", meta=(DisplayName="Get Monster Chase Speed"))
+	float GetChaseSpeed();
+	
+	UFUNCTION(BlueprintCallable, Category = "Status", meta=(DisplayName="Set Monster Chase Speed"))
+	void SetChaseSpeed(float NewSpeed);
+	
+#if WITH_EDITOR
+	
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+	
+protected:
+	virtual void OnConstruction(const FTransform& Transform) override;
+	virtual void BeginPlay() override;
+	
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	UStatusComponent* StatusComponent;
+	
+	
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Component")
+	UAudioComponent* AudioLoopComponent;
+	
+
+	
+private:
+	bool bIsExecutionActive = false;
+};
