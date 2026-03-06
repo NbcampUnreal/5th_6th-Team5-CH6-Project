466ae2115e48dab586580e33c3c3ae988a055c82
CHA <chayeongmin576@gmail.com>
2026-03-06 19:57:49 +0900

    차영민 2026/03/06 fix
    승원님 구현되지 안된 코드 주석처리

--- Ward_Zero/Source/Ward_Zero/Gimmick_SW/DoorLockTrigger.cpp
+++ Ward_Zero/Source/Ward_Zero/Gimmick_SW/DoorLockTrigger.cpp
@@ -1,43 +1,44 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Gimmick_SW/DoorLockTrigger.h"
 
 // Sets default values
 ADoorLockTrigger::ADoorLockTrigger()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
+	/*TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
 	TriggerBox->SetBoxExtent(FVector(300.0f, 300.0f, 200.0f));
 
 	TriggerBox->SetHiddenInGame(true);
 	TriggerBox->ShapeColor = FColor::Yellow;
 
 	bHasBeenTriggered = false;
-	bBossKilled = false;
+	bBossKilled = false;*/
 }
 
-// Called when the game starts or when spawned
 void ADoorLockTrigger::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	TriggerBox->OnComponentBeginOverlap.AddDynamic(
-		this, &ADoorLockTrigger::OnOverlapBegin);
+	//TriggerBox->OnComponentBeginOverlap.AddDynamic(
+	//	this, &ADoorLockTrigger::OnOverlapBegin);
 
 }
 
 void ADoorLockTrigger::BeginTriggered()
 {
-	if (OnOverlapBegin);
+	//if (OnOverlapBegin) 
+	
+
+	
 		//상호작용 기능 정지
 }
 
 void ADoorLockTrigger::EndTriggered()
 {
-	if (bBossKilled = true);
+	//if (bBossKilled = true);
+	
+
+	
 	//보스가 죽었을때 상호작용 기능 정상화
 }
 
--- Ward_Zero/Source/Ward_Zero/Gimmick_SW/DoorLockTrigger.h
+++ Ward_Zero/Source/Ward_Zero/Gimmick_SW/DoorLockTrigger.h
@@ -20,15 +20,15 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UFUNCTION()
-	void OnOverlapBegin(
-		UPrimitiveComponent* OverlappedComp,
-		AActor* OtherActor,
-		UPrimitiveComponent* OtherComp,
-		int32 OtherBodyIndex,
-		bool bFromSweep,
-		const FHitResult& SweepResult
-	);
+	//UFUNCTION()
+	//void OnOverlapBegin(
+	//	UPrimitiveComponent* OverlappedComp,
+	//	AActor* OtherActor,
+	//	UPrimitiveComponent* OtherComp,
+	//	int32 OtherBodyIndex,
+	//	bool bFromSweep,
+	//	const FHitResult& SweepResult
+	//);
 	
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
 	UBoxComponent* TriggerBox;
