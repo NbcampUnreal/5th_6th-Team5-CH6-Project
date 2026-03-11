02c77cf280cab267350ce3251ee79a4f80302308
inyy1224-coder <inyy1224@gmail.com>
2026-03-11 16:29:32 +0900

    윤찬영 2026/03/11 add
    슬라이딩 문 추가 & 문 메터리얼 분리

---
+++ Ward_Zero/Source/Ward_Zero/Gimmic_CY/SlidingDoorActor.cpp
@@ -0,0 +1,85 @@
+#include "Gimmic_CY/SlidingDoorActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/TimelineComponent.h"
+#include "Curves/CurveFloat.h"
+#include "Character/Prototype_Character/PrototypeCharacter.h"
+
+ASlidingDoorActor::ASlidingDoorActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	DoorFrame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorFrame"));
+	RootComponent = DoorFrame;
+
+	Door = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Door"));
+	Door->SetupAttachment(DoorFrame);
+
+	DoorTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("DoorTimeline"));
+
+}
+
+void ASlidingDoorActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ASlidingDoorActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ClosedLocation = Door->GetRelativeLocation();
+
+	UpdateFunctionFloat.BindDynamic(this, &ASlidingDoorActor::UpdateTimeline);
+
+	if (DoorCurve)
+	{
+		DoorTimeline->AddInterpFloat(DoorCurve, UpdateFunctionFloat);
+	}
+	
+}
+
+void ASlidingDoorActor::UpdateTimeline(float Value)
+{
+	FVector OpenLocation = ClosedLocation + OpenOffset;
+
+	FVector NewLocation = FMath::Lerp(ClosedLocation, OpenLocation, Value);
+
+	Door->SetRelativeLocation(NewLocation);
+}
+
+void ASlidingDoorActor::OnIneracted_Implementation(APrototypeCharacter* Character)
+{
+	if (IInteractionBase::Execute_CanBeInteracted(this))
+	{
+		IInteractionBase::Execute_HandleInteraction(this, Character);
+	}
+}
+
+void ASlidingDoorActor::HandleInteraction_Implementation(APrototypeCharacter* Character)
+{
+	if (!DoorCurve)
+		return;
+
+	if (!bIsOpen)
+	{
+		DoorTimeline->Play();
+	}
+	else
+	{
+		DoorTimeline->Reverse();
+	}
+
+	bIsOpen = !bIsOpen;
+}
+
+bool ASlidingDoorActor::CanBeInteracted_Implementation() const
+{
+	return true;
+}
+
+EInteractionType ASlidingDoorActor::GetInteractionType_Implementation() const
+{
+	return EInteractionType::Door;
+}
+
+
---
+++ Ward_Zero/Source/Ward_Zero/Gimmic_CY/SlidingDoorActor.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
+#include "Gimmic_CY/InteractionBase.h"
+#include "SlidingDoorActor.generated.h"
+
+class UStaticMeshComponent;
+class UTimelineComponent;
+class UCurveFloat;
+class APrototypeCharacter;
+
+UCLASS()
+class WARD_ZERO_API ASlidingDoorActor : public AActor, public IInteractionBase
+{
+	GENERATED_BODY()
+	
+public:	
+	ASlidingDoorActor();
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* DoorFrame;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Door;
+
+	UPROPERTY()
+	UTimelineComponent* DoorTimeline;
+
+	UPROPERTY(EditAnywhere)
+	UCurveFloat* DoorCurve;
+
+	FOnTimelineFloat UpdateFunctionFloat;
+
+	UFUNCTION()
+	void UpdateTimeline(float Value);
+
+	// À§Ä¡
+	FVector ClosedLocation;
+
+	UPROPERTY(EditAnywhere)
+	FVector OpenOffset = FVector(120.f, 0.f, 0.f);
+
+	bool bIsOpen = false;
+
+public:
+
+	// Interaction Interface
+	virtual void OnIneracted_Implementation(APrototypeCharacter* Character) override;
+	virtual void HandleInteraction_Implementation(APrototypeCharacter* Character) override;
+	virtual bool CanBeInteracted_Implementation() const override;
+	virtual EInteractionType GetInteractionType_Implementation() const override;
+
+};
