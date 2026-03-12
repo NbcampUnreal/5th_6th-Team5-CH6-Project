046c851da8de944fe4c4703b6e980707a18ac405
jsw0416 <j93319965@gmail.com>
2026-03-12 18:02:49 +0900

    정승원 2026/03/12 Add&Del
    기믹에 쓸 레버 추가&쓸모없는 파일 삭제

---
+++ Ward_Zero/Source/Ward_Zero/Gimmick_SW/Gimmick_Lever.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/TimelineComponent.h"
+#include "Gimmic_CY/InteractionBase.h"
+#include "Gimmick_Lever.generated.h"
+
+class UStaticMeshComponent;
+class UCurveFloat;
+class APrototypeCharacter;
+
+UCLASS()
+class WARD_ZERO_API AGimmick_Lever : public AActor, public IInteractionBase
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGimmick_Lever();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Lever_Frame;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Lever_Handle;
+
+	UPROPERTY(EditAnywhere)
+	UCurveFloat* LeverCurve;
+
+	FOnTimelineFloat UpdateFunctionFloat;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	virtual bool CanBeInteracted_Implementation() const override;
+	virtual void OnIneracted_Implementation(APrototypeCharacter* Character) override;
+};
